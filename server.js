1d0b2e4e282e9b91bf2e246f41767b2c17989a1a
Vinicius Duarte <vniciusdiniz@gmail.com>
2018-03-19 17:09:08 +1100

    initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express       = require('express');
+const hbs           = require('hbs');
+const fs            = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use(function (req, res, next){
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err){
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
+});
+
+// app.use(function (req, res, next){
+//     res.render('maintenance');
+// });
+
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get("/", function(req, res){
+    res.render( 'home', {
+        pageTitle: 'Home Page',
+        name: 'Andrew',
+        likes: [
+            'Biking',
+            'Cities'
+        ]
+       // currentYear: new Date().getFullYear()
+    });
+});
+
+app.get("/about", function(req, res){
+    res.render('about', {
+        pageTitle: 'About Page'
+       // currentYear: new Date().getFullYear()
+    });
+});
+
+app.get("/bad", function(req, res){
+    res.send({
+        error: 'Page not found.',
+    });
+});
+
+
+app.listen(3000, () => console.log('Server is up on port 3000'));
